748e7d9156b19fa2348b3ea097cd4a63979e0dbc
jucetina <juliethcetina@gmail.com>
2019-11-17 14:06:47 -0500

    Primer commit con todos los archivos del proyecto de la página web

---
+++ index.js
@@ -0,0 +1,5 @@
+$(function(){
+	//$('[data-toggle="tooltip"]') Selecciona el elemento de la página que tenga ese atributo
+	//Luego llama a la función tooltip()
+	$('[data-toggle="tooltip"]').tooltip();
+});
